14b43eafc94ec399782829f38c7e5be93e3659e0
Madu Carbonari <mariaeduarda.s.carbonari@gmail.com>
2019-05-27 22:17:57 -0300

    add

---
+++ js/addform.js
@@ -0,0 +1,4 @@
+function addform() {
+	var add = document.getElementById('formservico');
+	add.innerHTML += "<form class='servico' id='formservico'><h3>Serviço</h3><div class='sla'><label for='categoria'>Categoria</label><p><input type='text' id='categoria'></p><label for='especifico'>Específico</label><p><input type='text' id='especifico'></p><label for='descricao'>Descrição</label><p><textarea id='descricao'></textarea></p><label for='contato'>Contato</label><p><input type='number' id='contato'></p></div><div class='add'><button class='btn'><img class='btnadd' src='img/add.png'></button><button class='btn btnrelatoform'>Registrar</button></div></form>"
+}
